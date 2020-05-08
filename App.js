ae228e5749113d3f6f886ff7a17daeb4bc294ac4
Toni Melisma <toni.melisma@Tonis-MacBook-Pro.local>
2020-05-08 19:40:57 +0300

    Beginning refactoring...

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
-import MapView, { Marker, Callout } from "react-native-maps";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import MapView, { Marker, Callout } from "react-native-maps";
+import * as Location from "expo-location";
+
+// all map markers are consolidated in ready JSON data structure:
 import kioskit from "./markers/kioskit.json";
 
 const App = () => {
+  const [location, setLocation] = useState({
+    latitude: 60.169297,
+    longitude: 24.938435,
+    latitudeDelta: 0.03,
+    longitudeDelta: 0.03,
+  });
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      // if (status !== 'granted') {
+      //  setErrorMsg('Permission to access location was denied');
+      // }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.03,
+      });
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.mapStyle}>
+      <MapView style={styles.mapStyle} region={location}>
         {kioskit.map((marker) => (
           <Marker
             key={marker.id}
@@ -24,6 +54,7 @@ const App = () => {
             </Callout>
           </Marker>
         ))}
+        <Marker coordinate={location} pinColor="blue" />
       </MapView>
     </View>
   );
